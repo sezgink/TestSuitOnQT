8b9ae5d7a8915b5875c513624667c2d63329965b
sezgink <sezginkutlu00@gmail.com>
2024-11-24 23:38:15 +0300

    Create signal lib instance to prepeare it for processing incoming signals

--- TestSuite/TestSuite/main.cpp
+++ TestSuite/TestSuite/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-
+#include "signallib.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
+    SignalLib signalLib;
     w.show();
     return a.exec();
 }
