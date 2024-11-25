b0dd20852dcd033195efacad69fbf2fc5722377b
sezgink <sezginkutlu00@gmail.com>
2024-11-25 04:24:03 +0300

    Serial handler class for handling serial port data (WIP)

---
+++ TestSuite/TestSuite/serialhandler.cpp
@@ -0,0 +1,3 @@
+#include "serialhandler.h"
+
+SerialHandler::SerialHandler() {}
---
+++ TestSuite/TestSuite/serialhandler.h
@@ -0,0 +1,10 @@
+#ifndef SERIALHANDLER_H
+#define SERIALHANDLER_H
+
+class SerialHandler
+{
+public:
+    SerialHandler();
+};
+
+#endif // SERIALHANDLER_H
