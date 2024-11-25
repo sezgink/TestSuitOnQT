b2304c3dd3bb63fe484fced5bb5aae2248aa8143
sezgink <sezginkutlu00@gmail.com>
2024-11-25 22:26:02 +0300

    Serial data handler

--- TestSuite/TestSuite/serialhandler.cpp
+++ TestSuite/TestSuite/serialhandler.cpp
@@ -1,3 +1,126 @@
 #include "serialhandler.h"
 
-SerialHandler::SerialHandler() {}
+// Constructor: Initializes the QSerialPort and connects signals to slots
+SerialHandler::SerialHandler(QObject *parent)
+    : QObject(parent),
+    serial(new QSerialPort(this))
+{
+    // Connect the readyRead signal to handle incoming data
+    connect(serial, &QSerialPort::readyRead, this, &SerialHandler::handleReadyRead);
+
+    // Connect the errorOccurred signal to handle errors
+    connect(serial, &QSerialPort::errorOccurred, this, &SerialHandler::handleError);
+}
+
+// Destructor: Ensures the serial port is closed properly
+SerialHandler::~SerialHandler()
+{
+    if (serial->isOpen()) {
+        serial->close();
+    }
+}
+
+// Enumerate available serial ports
+QList<QSerialPortInfo> SerialHandler::availablePorts() const
+{
+    return QSerialPortInfo::availablePorts();
+}
+
+// Open a serial port with specified settings
+bool SerialHandler::openPort(const QString &portName, qint32 baudRate,
+                             QSerialPort::DataBits dataBits,
+                             QSerialPort::Parity parity,
+                             QSerialPort::StopBits stopBits,
+                             QSerialPort::FlowControl flowControl)
+{
+    if (serial->isOpen()) {
+        serial->close();
+    }
+
+    serial->setPortName(portName);
+    serial->setBaudRate(baudRate);
+    serial->setDataBits(dataBits);
+    serial->setParity(parity);
+    serial->setStopBits(stopBits);
+    serial->setFlowControl(flowControl);
+
+    if (!serial->open(QIODevice::ReadWrite)) {
+        emit errorOccurred(QString("Failed to open port %1: %2")
+                               .arg(portName, serial->errorString()));
+        return false;
+    }
+
+    return true;
+}
+
+// Close the currently open port
+void SerialHandler::closePort()
+{
+    if (serial->isOpen()) {
+        serial->close();
+    }
+}
+
+// Check if the port is open
+bool SerialHandler::isOpen() const
+{
+    return serial->isOpen();
+}
+
+// Handle incoming data when readyRead signal is emitted
+void SerialHandler::handleReadyRead()
+{
+    QByteArray data = serial->readAll();
+    if (!data.isEmpty()) {
+        buffer.append(data);
+
+        //Data will process here WIP
+
+        emit newDataReceived(buffer);
+        buffer.clear();
+    }
+}
+
+// Handle errors when errorOccurred signal is emitted
+void SerialHandler::handleError(QSerialPort::SerialPortError error)
+{
+    if (error == QSerialPort::NoError) {
+        return;
+    }
+
+    QString errorMessage;
+
+    switch (error) {
+    case QSerialPort::DeviceNotFoundError:
+        errorMessage = "Device not found.";
+        break;
+    case QSerialPort::PermissionError:
+        errorMessage = "Permission denied.";
+        break;
+    case QSerialPort::OpenError:
+        errorMessage = "Unable to open the port.";
+        break;
+    case QSerialPort::ReadError:
+        errorMessage = "Read error.";
+        break;
+    case QSerialPort::ResourceError:
+        errorMessage = "Resource error.";
+        break;
+    case QSerialPort::UnsupportedOperationError:
+        errorMessage = "Unsupported operation.";
+        break;
+    case QSerialPort::UnknownError:
+    default:
+        errorMessage = serial->errorString();
+        break;
+    }
+
+    emit errorOccurred(errorMessage);
+
+    // Close the port on certain errors
+    if (serial->isOpen() && (error == QSerialPort::ResourceError ||
+                             error == QSerialPort::UnsupportedOperationError ||
+                             error == QSerialPort::DeviceNotFoundError)) {
+        serial->close();
+    }
+}
--- TestSuite/TestSuite/serialhandler.h
+++ TestSuite/TestSuite/serialhandler.h
@@ -1,10 +1,45 @@
-#ifndef SERIALHANDLER_H
-#define SERIALHANDLER_H
+#pragma once
 
-class SerialHandler
+#include <QObject>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QVector>
+
+class SerialHandler : public QObject
 {
+    Q_OBJECT
 public:
-    SerialHandler();
-};
+    explicit SerialHandler(QObject *parent = nullptr);
+    ~SerialHandler();
 
-#endif // SERIALHANDLER_H
+    // Enumerate available serial ports
+    QList<QSerialPortInfo> availablePorts() const;
+
+    // Open a serial port with specified settings
+    bool openPort(const QString &portName, qint32 baudRate = 115200,
+                  QSerialPort::DataBits dataBits = QSerialPort::Data8,
+                  QSerialPort::Parity parity = QSerialPort::NoParity,
+                  QSerialPort::StopBits stopBits = QSerialPort::OneStop,
+                  QSerialPort::FlowControl flowControl = QSerialPort::NoFlowControl);
+
+    // Close the currently open port
+    void closePort();
+
+    // Check if the port is open
+    bool isOpen() const;
+
+signals:
+    // Emitted when new data is received
+    void newDataReceived(const QByteArray &data);
+
+    // Emitted when an error occurs
+    void errorOccurred(const QString &errorString);
+
+private slots:
+    void handleReadyRead();
+    void handleError(QSerialPort::SerialPortError error);
+
+private:
+    QSerialPort *serial;
+    QByteArray buffer;
+};
